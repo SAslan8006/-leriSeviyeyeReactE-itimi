b864b457e7b6dc5293408e51df2b02d06df37ebc
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-04-26 09:58:03 +0300

    ssss

--- reactredux/src/main.jsx
+++ reactredux/src/main.jsx
@@ -1,6 +1,4 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Product from './pages/Product.jsx';
