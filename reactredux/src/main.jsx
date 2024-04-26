85df36255e6f5403e775987540e4c9c473f5bd6b
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-04-26 09:57:32 +0300

    React Redux Footer ve Header yapıs Proje paketleri yüklenmiştir.

---
+++ reactredux/src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Product from './pages/Product.jsx';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route index path="/" element={<Product />} />
+    </Routes>
+    <Footer />
+  </BrowserRouter>
+)
+
+
+// Header her sayfada olacağı için buraya kaydedilir. Aynı sırada footer da kaydedilir.
---
+++ reactredux/src/pages/Product.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header'
+import ProductCard from '../components/ProductCard'
+import Footer from '../components/Footer'
+
+const Product = () => {
+    return (
+        <div>
+            <ProductCard />
+            <Footer />
+        </div>
+    )
+}
+
+export default Product
